a68efe4928e1c3f1d3bf6ae2114d531633fd62f2
Oskar <118886547+Oskar090@users.noreply.github.com>
2023-03-06 10:33:39 +0100

    1.0.5
    Adding records to database

--- serwer/index.js
+++ serwer/index.js
@@ -18,29 +18,45 @@ var con = mysql.createConnection({
 })
 
 //ERROR
-con.connect(function(err){
-    if(err){
-    console.log(err)
-    } else console.log("Połączo z bazą danych") 
+con.connect(function (err) {
+    if (err) {
+        console.log(err)
+    } else console.log("Połączo z bazą danych")
 })
 
 //Czy działa; odp w konsoli
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("ok")
 })
 
-//Zapytanie do bazy danych 
-app.get("/select", (req,res)=>{
+//Zapytanie do bazy danych i wyświetlenie danych
+app.get("/select", (req, res) => {
     const sql = "SELECT * FROM uczniowie_budul"
-    con.query(sql, function(err, result, fileds){ //błąd, resultaty, informacje o danych
-        if (err){
+    con.query(sql, function (err, result, fileds) { //błąd, resultaty, informacje o danych
+        if (err) {
             console.log(err)
         } else res.send(result)
     })
 })
 
+//Zapisywanie danych w bazie danych 
+app.get("/add/:imie/:nazwisko/:klasa", (req, res) => {
+    const imie = req.params.imie
+    const nazwisko = req.params.nazwisko
+    const klasa = req.params.klasa
+
+    const sql = `INSERT INTO uczniowie_budul (imie, nazwisko, klasa) VALUES ('${imie}','${nazwisko}','${klasa}')`
+    con.query(sql, function (err, result, fileds) {
+        if (err) {
+            console.log(err)
+        } else res.send("Dodano rekord")
+    })
+})
+
+//http://localhost:3000/add/Dariusz/Kr%C3%B3l/1PRO
+
 //Czy działa; odp w terminalu
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("Aplikacja działa na porcie:" + port)
 })
 
